528d66b09b427204049aa69ddc45e4866c935b5a
zenzontle <zenzontle@gmail.com>
2016-01-17 21:36:41 -0700

    Module 3. Javascript

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿// site.js
+(function () {
+
+    //var ele = $("#username");
+    //ele.text("Jorge Hernandez");
+
+    //var main = $("#main");
+    //main.on("mouseenter", function () {
+    //    main.css("background-color", "#888");
+    //});
+    //main.on("mouseleave", function () {
+    //    main.css("background-color", "");
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+
+    //    alert(me.text());
+    //});
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar"))
+        {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+})();
